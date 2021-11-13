02e5c9a65037f7396413d9ab4aee9e79f7e1ac78
S7ase7o <muhamedhussein7@gmail.com>
2021-11-13 23:20:44 +0200

    first

---
+++ precache-manifest.6a47b5e462656910f667582618f39385.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84d1ec8fb03ee8d924b46aa16776b0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53ffd5df53af76b1aaf",
+    "url": "/static/css/main.8894e24e.chunk.css"
+  },
+  {
+    "revision": "82ad54eb63dfc0462a17",
+    "url": "/static/js/2.e62a1aae.chunk.js"
+  },
+  {
+    "revision": "e53ffd5df53af76b1aaf",
+    "url": "/static/js/main.947fc826.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
